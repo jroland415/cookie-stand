e573e8f70d3c4cc29cd906ef31e1d78d75e18dfa
Jessica Roland <jroland015004@gmail.com>
2018-12-03 14:09:29 -0800

    Initial setup of location objects

---
+++ js/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', 'Total'];
+
+var firstAndPike = {
+  storeName: '1st and Pike',
+  minCust: 23,
+  maxCust: 65,
+  avgCookie: 6.3,
+}
+
+var seaTac = {
+  storeName: 'SeaTac Airport',
+  minCust: 3,
+  maxCust: 24,
+  avgCookie: 1.2,
+}
+
+var seattleCenter = {
+  storeName: 'Seattle Center',
+  minCust: 11,
+  maxCust: 38,
+  avgCookie: 3.7,
+}
+
+var capitolHill = {
+  storeName: 'Capitol Hill',
+  minCust: 20,
+  maxCust: 38,
+  avgCookie: 2.3,
+}
+
+var alki = {
+  storeName: 'Alki',
+  minCust: 2,
+  maxCust: 16,
+  avgCookie: 4.6,
+}
