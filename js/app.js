81c6af9711c67993c529ede5e2b5d8a0b83e6014
Jessica Roland <jroland015004@gmail.com>
2018-12-05 13:26:40 -0800

    Added basic form to Sales page

--- js/app.js
+++ js/app.js
@@ -4,12 +4,14 @@ var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2p
 
 var cookieTable = document.getElementById('cookie-table');
 Cookie.allStores = [];
+var cookieForm = document.getElementById('new-location-form');
 
 function Cookie(storeName, minCust, maxCust, avgCookie) {
   this.storeName = storeName;
   this.minCust = minCust;
   this.maxCust = maxCust;
   this.avgCookie = avgCookie;
+  this.totalSoldStore = 0;
   Cookie.allStores.push(this);
 }
 
@@ -33,14 +35,11 @@ Cookie.prototype.render = function() {
   tdEl.textContent = this.storeName;
   trEl.appendChild(tdEl);
 
-  var totalSoldStore = 0;
-  //var byTheHour = [];
   for(var i = 0; i < storeHours.length - 1; i++) {
     var numCustomers = Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
 
     var numCookies = Math.ceil(numCustomers * this.avgCookie);
-    totalSoldStore += numCookies;
-    //byTheHour.push(numCookies);    create array to store random values?
+    this.totalSoldStore += numCookies;
 
     tdEl = document.createElement('td');
     tdEl.textContent = numCookies;
@@ -48,16 +47,30 @@ Cookie.prototype.render = function() {
   }
   tdEl = document.createElement('td');
   tdEl.id = 'total-per-store';
-  tdEl.textContent = totalSoldStore;
+  tdEl.textContent = this.totalSoldStore;
   trEl.appendChild(tdEl);
   cookieTable.appendChild(trEl);
 };
 
-new Cookie('1st and Pike', 23, 65, 6.3);
-new Cookie('SeaTac', 3, 24, 1.2);
-new Cookie('Seattle Center', 11, 38, 3.7);
-new Cookie('Capitol Hill', 20, 38, 2.3);
-new Cookie('Alki', 2, 16, 4.6);
+Cookie.addNewLocation = function(event) {
+  event.preventDefault();
+
+  var newLocation = event.target.locationName.value;
+  var newMin = event.target.minPerHour.value;
+  var newMax = event.target.maxPerHour.value;
+  var newAvg = event.target.avgCook.value;
+  var newCookieStand = new Cookie(newLocation, newMin, newMax, newAvg);
+
+  cookieTable.textContent = '';
+  Cookie.renderHeader();
+  Cookie.renderAllStores();
+};
+
+var firstAndPike = new Cookie('1st and Pike', 23, 65, 6.3);
+var seaTac = new Cookie('SeaTac', 3, 24, 1.2);
+var seattleCenter = new Cookie('Seattle Center', 11, 38, 3.7);
+var capitolHill = new Cookie('Capitol Hill', 20, 38, 2.3);
+var alki = new Cookie('Alki', 2, 16, 4.6);
 
 Cookie.renderAllStores = function() {
   for(var i = 0; i < Cookie.allStores.length; i++) {
@@ -67,3 +80,5 @@ Cookie.renderAllStores = function() {
 
 Cookie.renderHeader();
 Cookie.renderAllStores();
+
+cookieForm.addEventListener('submit', Cookie.addNewLocation);
